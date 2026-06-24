5ef0c74a9e5e17b2919bd79df408b799ef6704f3
Danila <rexerofficial@gmail.com>
2026-06-24 13:30:58 +0300

    Initial commit

---
+++ app/src/main/java/ru/malevichrp/financetracker/MainActivity.kt
@@ -0,0 +1,28 @@
+package ru.malevichrp.financetracker
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import ru.malevichrp.financetracker.ui.theme.FinanceTrackerTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            FinanceTrackerTheme {
+                FinanceApp()
+            }
+        }
+    }
+}
+
+@Composable
+fun FinanceApp() {
+
+}
